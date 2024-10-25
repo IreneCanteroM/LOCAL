dd4c82d8ec17a592c2038765d461bd5d7740aa93
DAFG-Works <140922558+DAFG-Works@users.noreply.github.com>
2024-10-25 18:53:06 +0200

    Parte 2 de update mezclados

--- local/JS/script.js
+++ local/JS/script.js
@@ -24,46 +24,57 @@ navbarCollapse.addEventListener('hide.bs.collapse', function () {
 });
 
 
-// checkboxes
-const items = [
-  { checkboxId: "calamares", imageId: "bl-1", color: "red" },
-  { checkboxId: "jamon", imageId: "bl-2", color: "yellow" },
-  { checkboxId: "churros", imageId: "bl-3", color: "green" },
-  { checkboxId: "rastro", imageId: "bl-4", color: "blue" },
-  { checkboxId: "cocido", imageId: "bl-5", color: "orange" },
-  { checkboxId: "granvia", imageId: "bl-6", color: "orange" },
-  { checkboxId: "tortilla", imageId: "bl-7", color: "red" },
-  { checkboxId: "torres", imageId: "bl-8", color: "blue" },
-  { checkboxId: "palacio", imageId: "bl-9", color: "green" },
-  { checkboxId: "retiro", imageId: "bl-10", color: "yellow" },
-  { checkboxId: "metro", imageId: "bl-11", color: "yellow" },
-  { checkboxId: "prado", imageId: "bl-12", color: "green" },
-  { checkboxId: "plaza", imageId: "bl-13", color: "orange" },
-  { checkboxId: "oso", imageId: "bl-14", color: "red" },
-  { checkboxId: "puerta", imageId: "bl-15", color: "blue" }
-];
 
-function toggleColor(checkbox, image, color) {
-  checkbox.addEventListener("change", function() {
-    if (this.checked) {
-      image.classList.add(color);
-    } else {
-      image.classList.remove(color);
-    }
+// Solo ocurre en el index.html
+// tenemos que detectar en que página estamos
+// SI estamos en index.html todo este bloque de código se tiene que activar
+
+if () {
+  // checkboxes
+  const items = [
+    { checkboxId: "calamares", imageId: "bl-1", color: "red" },
+    { checkboxId: "jamon", imageId: "bl-2", color: "yellow" },
+    { checkboxId: "churros", imageId: "bl-3", color: "green" },
+    { checkboxId: "rastro", imageId: "bl-4", color: "blue" },
+    { checkboxId: "cocido", imageId: "bl-5", color: "orange" },
+    { checkboxId: "granvia", imageId: "bl-6", color: "orange" },
+    { checkboxId: "tortilla", imageId: "bl-7", color: "red" },
+    { checkboxId: "torres", imageId: "bl-8", color: "blue" },
+    { checkboxId: "palacio", imageId: "bl-9", color: "green" },
+    { checkboxId: "retiro", imageId: "bl-10", color: "yellow" },
+    { checkboxId: "metro", imageId: "bl-11", color: "yellow" },
+    { checkboxId: "prado", imageId: "bl-12", color: "green" },
+    { checkboxId: "plaza", imageId: "bl-13", color: "orange" },
+    { checkboxId: "oso", imageId: "bl-14", color: "red" },
+    { checkboxId: "puerta", imageId: "bl-15", color: "blue" }
+  ];
+  
+  function toggleColor(checkbox, image, color) {
+    checkbox.addEventListener("change", function() {
+      if (this.checked) {
+        image.classList.add(color);
+      } else {
+        image.classList.remove(color);
+      }
+    });
+  
+    image.addEventListener("click", function() {
+      this.classList.toggle(color);
+      this.classList.toggle("img-bl-hover");
+      checkbox.checked = !checkbox.checked;
+      
+    });
+  }
+  
+  items.forEach(item => {
+    const checkbox = document.getElementById(item.checkboxId);
+    const image = document.getElementById(item.imageId);
+    toggleColor(checkbox, image, item.color);
   });
+}  
 
-  image.addEventListener("click", function() {
-    this.classList.toggle(color);
-    this.classList.toggle("img-bl-hover");
-    checkbox.checked = !checkbox.checked;
-    
-  });
-}
 
-items.forEach(item => {
-  const checkbox = document.getElementById(item.checkboxId);
-  const image = document.getElementById(item.imageId);
-  toggleColor(checkbox, image, item.color);
-});
+
+
 
 
